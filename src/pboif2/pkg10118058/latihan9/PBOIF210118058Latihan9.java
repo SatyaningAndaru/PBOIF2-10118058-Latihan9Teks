f50c4e314548f5b5c91531ff59521ffa70de49cb
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 18:24:46 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan9/PBOIF210118058Latihan9.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan9;
+
+/**
+ *
+ * @author user
+ * * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline= '\n';
+        c = 'A';
+        System.out.println("Hasil"+newline +"Char : " + tab +"Berisi  " + c);    
+             
+        
+    }
+    
+}
